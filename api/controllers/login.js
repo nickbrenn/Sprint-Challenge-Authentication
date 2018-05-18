0592d91b86420f2e29748f657e2f039c91a7c816
nickbrenn <nbren@tutanota.com>
2018-05-18 14:59:36 -0400

    Got registering, logging in and viewing jokes using tokens to work

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -1,22 +1,24 @@
-const jwt = require('jsonwebtoken');
-const { mysecret } = require('../../config');
-const User = require('../models/userModels');
+const jwt = require("jsonwebtoken");
+const { mysecret } = require("../../config");
+const User = require("../models/userModels");
 
 const login = (req, res) => {
-  const { username, password } = req.body;
+  let { username, password } = req.body;
+  username = username.toLowerCase();
   User.findOne({ username }, (err, user) => {
     if (err) {
-      res.status(403).json({ error: 'Invalid Username/Password' });
+      res.status(403).json({ error: "Invalid Username/Password" });
       return;
     }
     if (user === null) {
-      res.status(422).json({ error: 'No user with that username in our DB' });
+      res.status(422).json({ error: "No user with that username in our DB" });
       return;
     }
     user.checkPassword(password, (nonMatch, hashMatch) => {
       // This is an example of using our User.method from our model.
-      if (nonMatch !== null) {
-        res.status(422).json({ error: 'passwords dont match' });
+      if (!hashMatch) {
+        // Couldn't get this error to ever appear unless I made it !hashMatch
+        res.status(422).json({ error: "passwords dont match" });
         return;
       }
       if (hashMatch) {
--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,10 +1,21 @@
-const User = require('../models/userModels');
-const bcrypt = require('bcrypt');
+const User = require("../models/userModels");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+const secret = "secret = safe";
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
-  // create user takes in the username and password and saves a user.
-  // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  const user = new User({ username, password });
+  return user.save((err, user) => {
+    if (err) return res.send(err);
+    const token = getTokenForUser({ username: user.username });
+    res.json({ token });
+  });
+};
+
+const getTokenForUser = user => {
+  return jwt.sign(user, secret, { expiresIn: "1h" });
 };
 
 module.exports = {
